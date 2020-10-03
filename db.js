e84e2700b17b3e4d7210eb311e03c64c2b33d6af
Kerlly Figueroa <kfigueroaa@est.ups.edu.ec>
2020-10-02 19:26:16 -0500

    Actualización

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ const db = require('mongoose')
 db.Promise = global.Promise
 
 async function connect() {
-    const uri = "mongodb+srv://ups:ups2020@cluster0.pwmqh.mongodb.net/utups?retryWrites=true&w=majority"
+    const uri = "mongodb+srv://ups:12345@cluster0.vtrih.gcp.mongodb.net/test"
     await db.connect(uri, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        dbName: 'utups',
+        dbName: 'upsdb',
     })
         .then((data) => { console.log('[db] - Conectada con éxito.') })
         .catch((error) => {  console.error( '[error log] - ' + error ) })    
